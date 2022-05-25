e85ec61ee999e9cee8058b7cdab45c9af58521e0
jaseyrae <jcreysalatan@gmail.com>
2022-05-25 23:04:08 +0800

    server file update

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
@@ -10,6 +12,11 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err);
 })
 
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is ready!");
 }); 
+  
