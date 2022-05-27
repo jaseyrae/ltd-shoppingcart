7bf710599bcdfc11ebd659a501e6e562c027ff91
jaseyrae <jcreysalatan@gmail.com>
2022-05-27 20:29:04 +0800

    updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res) =>{
@@ -31,10 +32,18 @@ router.post("/login", async (req, res) => {
 
         mainPassword !== req.body.password && res.status(401).json("Worng credentials!");
 
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+
         // para di makita ang password sa response
         const { password, ...others } = user._doc; 
 
-        res.status(200).json(others);
+
+        res.status(200).json({...others, accessToken});
     } catch (err) {
         res.status(500).json(err);
     }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,39 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
+// UPDATE
+router.put("/:id",verifyTokenAndAuthorization, async (req,res)=>{
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toSting();
+    }
 
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedUser);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+
+//  DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("user has been deleted!!");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
